e544c94edd8d05fcd7e720ca4d1c66cb6ca10ab5
KitaharaMugiro <you@example.com>
2021-12-01 18:47:00 +0900

    カーソルにカスタム情報を追加できるように修正

--- pages/realtime-cursor-test.tsx
+++ pages/realtime-cursor-test.tsx
@@ -1,24 +1,34 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RealtimeHeader from '../components/RealtimeHeader';
 import { useRealtimeCursor } from '../src';
 import styles from '../styles/Home.module.css';
-
+import { v4 as uuidv4 } from "uuid";
+import { CursorAnimate, CustomCursorViewParameter } from '../src/components/CursorAnimate';
+import { Cursor } from '../src/components/Cursor';
 const Home: NextPage = () => {
-    const { onMouseMove, renderCursors } = useRealtimeCursor(100)
+    const [customUserInfo, setCustomeUserInfo] = useState({ name: "", id: "" })
+    const { onMouseMove, renderCursors } = useRealtimeCursor(100, JSON.stringify(customUserInfo))
+
+    useEffect(() => {
+        const id = uuidv4()
+        const name = id.substring(0, 5)
+        setCustomeUserInfo({ name, id })
+    }, [])
 
     /**カスタムビューを設定したい場合 */
-    // const customView = (param: CustomCursorViewParameter) => {
-    //     return (<CursorEye userInfo={param.userInfo} />)
-    // }
+    const customView = (param: CustomCursorViewParameter) => {
+        console.log({ customInfo: param.customInfo })
+        return (<Cursor userInfo={param.userInfo} />)
+    }
 
     return (
         <div className={styles.container} onMouseMove={onMouseMove}>
             <RealtimeHeader />
             <main className={styles.main}>
-                {renderCursors()}
+                {renderCursors(customView)}
                 <h1 className={styles.title}>
                     Hey! Let's talk in <Link href="/text-field"><a>Realtime Chat</a></Link>
                 </h1>
